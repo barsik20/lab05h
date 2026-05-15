5a69c09f3a8900c71b17f571e3000215a6bf5c9f
barsik20 <124285675+barsik20@users.noreply.github.com>
2026-05-15 20:16:57 +0300

    Delete _build directory

--- _build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/barsik20/workspace/projects/lab05h/third-party/gtest
-# Build directory: /root/barsik20/workspace/projects/lab05h/_build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
