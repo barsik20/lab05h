9da86b2e6d71616152403766de59a4936d3b2286
barsik20 <bars.070620077777@gmail.com>
2026-05-15 20:14:35 +0300

    Removed all _build directories and updated .gitignore

---
+++ _build/third-party/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/barsik20/workspace/projects/lab05h/third-party/gtest/googlemock
+# Build directory: /root/barsik20/workspace/projects/lab05h/_build/third-party/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
